f4c4ba7a1ac1abda5e89fda7d2f4ad9120ebcaea
zhongti0126 <281444514+zhongti0126@users.noreply.github.com>
2026-05-06 23:37:24 +0800

    add valid parentheses solution
    Implement a solution to validate parentheses.

---
+++ Day1/valid_parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+  def isValid(self, s):
+
+      stack = []
+
+      for char in s:
+        if char == '(':
+          stack.append(char)
+
+      else: 
+        if not stack:
+          return False
+
+        stack.pop()
+
+    return len(stack) == 0
